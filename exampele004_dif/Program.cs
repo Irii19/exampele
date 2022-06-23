bc91e15fb252553bd5f815fe4fe32ebcc4e66265
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-23 17:31:22 +0300

    fourth program 2

--- exampele004_dif/Program.cs
+++ exampele004_dif/Program.cs
@@ -1,9 +1,17 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
 {
     private static void Main(string[] args)
     {
         double numberA = 18;
         double numberB = 5;
         Console.WriteLine(numberA / numberB);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
