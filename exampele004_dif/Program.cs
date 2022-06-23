3df3936588d873c7c1eebffbe04af26d13817eda
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-23 17:07:18 +0300

    fourth program

---
+++ exampele004_dif/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numberA = 18;
+        double numberB = 5;
+        Console.WriteLine(numberA / numberB);
+    }
+}
---
+++ seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int squareNumber = number * number;
+
+Console.WriteLine("квадрат числа равен: "  + squareNumber);
