ee31013ea4d6a2ced974ea4bc5ba8ef7c111485c
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-23 18:32:33 +0300

    sixth program

---
+++ exampele006_conditions/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Приветствую ВАС, введите ВАШЕ имя");
+string username = Console.ReadLine( );
+
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("Мы тебя так долго ждали. МАША");
+}
+else 
+{
+Console.WriteLine("Привет!!! " + username );
+}
