8122ab845471a4f9295a1e5f0e606b5ec2487d43
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-24 23:24:07 +0300

    проверка на сооьветствия двух чисел на их корневость

---
+++ seminar2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine ("у тебя все получится!!!");
+
+Console.WriteLine ("ведите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine ());
+
+Console.WriteLine ("ведите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine ());
+
+if (numberA == numberB * numberB)
+{
+Console.WriteLine ("второе число является квадратом первого");
+}
+else 
+{
+    Console.WriteLine ("второе число не является квадратом певого");
+}
