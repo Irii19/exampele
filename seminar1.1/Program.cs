4a2086c5c8a9a1be42e30d42ce11af206ed4939b
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-24 22:48:59 +0300

    квадрат целого числа

---
+++ seminar1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("здраствуйте, введите целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine(square);
