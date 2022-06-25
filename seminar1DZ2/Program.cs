7c0b0fb4b237b369022378bac3a8ba8a62cae572
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-26 00:04:44 +0300

    определить четное или не четное число

---
+++ seminar1DZ2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine();
+Console.WriteLine("                           все будет хорошо");
+Console.WriteLine();
+Console.WriteLine("программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine();
+
+Console.WriteLine("ведите первое (целое) число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе (целое) число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите третье (целое) число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(numberA > numberB)
+{
+    if(numberA > numberC)
+    {
+        Console.WriteLine(numberA);
+    }
+    else
+    {
+        Console.WriteLine(numberC);
+    }
+
+else
+
+    if(numberB > numberC)
+    {
+        Console.WriteLine(numberB);
+    }
+    else
+    {
+        Console.WriteLine(numberC);
+    }
+    
+}
---
+++ seminar1DZ3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("               вперед, вперед и только в перед!!!");
+
+Console.WriteLine("программа, которая на вход принимает число и выдаёт, является ли число чётным");
+
+
+Console.WriteLine("Введите число: ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (c % 2 == 0) 
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
