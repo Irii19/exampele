e2a848c751d1eb626a294d99de315265838f45a9
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 00:52:14 +0300

    на вход трёхзначное число и на выходе показывает последнюю цифру этого числа

---
+++ seminar1.5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.");
+
+Console.WriteLine();
+Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+{
+    int a = number % 10;
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("не верно ведены данные");
+}
+
+
+
+
+
+
