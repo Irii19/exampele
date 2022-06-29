ee8e9cec25a055512371b088a67b275e4bd78629
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 22:31:32 +0300

    программf, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.ЛОГИЧЕСКОЕ РЕШЕНИЕ  BOOL

---
+++ reminar2.3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+// КОРОТКИЙ ВАРИАНТ логическогорешения задачи
+
+
+// bool IsMultiplicity (int number)
+// {
+//     return(number%7==0 && number%23==0);
+// }
+// Console.WriteLine(IsMultiplicity (46));
+
+
+
+int number = 1127;
+
+bool IsMultiplicity (int number)
+{
+    return(number%7==0 && number%23==0);
+}
+Console.WriteLine(IsMultiplicity (number));
