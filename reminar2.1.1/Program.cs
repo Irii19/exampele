fb514c3592930b76396d33cc2e43d7d1917c1083
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 20:37:05 +0300

    которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.

---
+++ reminar2.1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.");
+
+Console.WriteLine();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int SearchResult(int number){
+    int first = number / 100;
+    int third = number % 10;
+    return first*10 + third;}
+Console.WriteLine(SearchResult(number));
+
