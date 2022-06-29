ab56583628061e9518fbf477568bdae23ef520ae
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 13:10:29 +0300

    выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа с фунцией

---
+++ reminar2.0.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number = new Random().Next(10,100);
+Console.WriteLine($"случайное число - {number}");
+
+int digitOne = number /10;
+int digitTwo = number % 10;
+Console.WriteLine(digitOne);
+Console.WriteLine(digitTwo);
+
+int max = Math.Max(digitOne, digitTwo);
+Console.WriteLine($"наибольшее число: {max}, кто не понял, наибольшее число {max}");
