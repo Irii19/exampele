67eda390acc733b7c4f7507b45d67b663c6f3b9f
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 23:19:55 +0300

    программ, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.

---
+++ reminar2DZ1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+Console.WriteLine();
+Console.WriteLine("введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>= 1 && number<= 5)
+{
+    Console.WriteLine("этот день не выходной");
+}
+else
+{
+    Console.WriteLine("этот день выходной");
+}
