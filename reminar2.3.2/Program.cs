ca7f0e4d5bb5048e37bfc248c92278838d809670
Iurii_Bobrov <bobzavr@yandex.ru>
2022-06-29 22:56:42 +0300

    программf, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.ЛОГИЧЕСКОЕ РЕШЕНИЕ  BOOL + гениратор случайных чисел

--- reminar2.3.2/Program.cs
+++ reminar2.3.2/Program.cs
@@ -15,10 +15,29 @@ Console.WriteLine("Hello, World!");
 
 
 
-int number = 1127;
+// МОЖНО И ТАК, через консоль
+
+
+// Console.WriteLine("введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool IsMultiplicity (int number)
+// {
+//     return(number%7==0 && number%23==0);
+// }
+// Console.WriteLine(IsMultiplicity (number));
+
+
+// МОЖНО И ТАК, ЧЕРЕЗГЕНЕРАТОР СЛУЧАЙНЫХ ЧИСЕЛ
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
 
 bool IsMultiplicity (int number)
 {
     return(number%7==0 && number%23==0);
 }
 Console.WriteLine(IsMultiplicity (number));
+
+
