a0641e42d744b09e2e914c3e59b0611c022a08eb
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-03 15:11:12 +0300

    нахождение элемента в массиве

---
+++ exampele0010/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = {1, 12, 4, 48, 4, 15, 26, 37, 48};
+
+int n = array.Length;
+int find = 48; 
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+         Console.WriteLine(index);
+         break;
+    }
+
+    index++;
+}
