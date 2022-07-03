1c170728ff2a375bc48e68812aedc909b65b8086
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-03 10:41:15 +0300

    фрактальное изображение треугольника

--- exampele008_while/Program.cs
+++ exampele008_while/Program.cs
@@ -42,3 +42,18 @@
 //     count = count + 1;
 // }
 
+Console.Clear();
+int xa = 1;
+int ya = 1;
+
+int xb = 1, yb = 15,
+    xc = 40, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("$");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
