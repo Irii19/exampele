7374c1d9ea02e3757132f08a5ee618ff450cdae9
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-03 22:23:18 +0300

    17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X  0 и Y  0 и выдаёт номер четверти плоскости, в которой находится эта точка.

---
+++ seminar3.1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("введите число");
+// int n =Convert.ToInt32(Console.ReadLine());
+
+// if(int x = 0; x>5; x++)
+// {
+//     if(n==1){
+//         Console.WriteLine("x>0 , y>0");
+//     }
+//     if(n==2){
+//         Console.WriteLine("x<0 , y>0");
+//     }
+//     if(n==3){
+//         Console.WriteLine("x<0 , y<0");
+//     }
+//     if(n==4){
+//         Console.WriteLine("x>0 , y<0");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("нет такой четверти");
+// }
+
+Console.WriteLine("введите число к коодинату x ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число в координату y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
--- seminar3.1/obj/Debug/net6.0/seminar3.1.AssemblyInfo.cs
+++ seminar3.1/obj/Debug/net6.0/seminar3.1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("seminar4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("seminar3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("seminar4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("seminar4")]
+[assembly: System.Reflection.AssemblyProductAttribute("seminar3.1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("seminar3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
