280256931c0eb672ae8231c7e6ad7311a96665c9
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-07 00:41:09 +0300

    Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.

---
+++ seminar4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 5 -> 120
+
+System.Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for(int i = 1; i<=N; i++)
+{
+    result*=i;
+}
+System.Console.WriteLine(result);
