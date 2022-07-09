2e12980f4c3c7d7f33ab5017ed1583c2121de0ce
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-09 11:56:55 +0300

    измениения с 4 лекции.

--- seminar4.2/Program.cs
+++ seminar4.2/Program.cs
@@ -5,8 +5,8 @@
 // Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24 5 -> 120
 
-// System.Console.WriteLine("введите число");
-// int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
 
 // int result = 1;
 // for(int i = 1; i<=N; i++)
@@ -14,6 +14,17 @@
 //     result*=i;
 // }
 // Console.WriteLine(result);
+
+int result = 1;
+int i = 1;
+
+while(i<=N){
+    result*=i;
+    i++;
+}
+Console.WriteLine(result);
+
+
 
 
 
