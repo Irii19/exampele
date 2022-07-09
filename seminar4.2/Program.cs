3046ffbe9cab35f244450cace67e316aeff60d1c
Iurii_Bobrov <bobzavr@yandex.ru>
2022-07-09 11:36:07 +0300

    дополнени с 4 лекции.

--- seminar4.2/Program.cs
+++ seminar4.2/Program.cs
@@ -1,15 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+
+
 // Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24 5 -> 120
 
-System.Console.WriteLine("введите число");
-int N = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-int result = 1;
+// int result = 1;
+// for(int i = 1; i<=N; i++)
+// {
+//     result*=i;
+// }
+// Console.WriteLine(result);
 
-for(int i = 1; i<=N; i++)
-{
-    result*=i;
-}
-System.Console.WriteLine(result);
+
+
+////// СЕМИНАР 4. 
+////// ----------
+
+
+//// 4. ВЫВОД МАССИВА В ТЕРМИНАЛ.
+// int [] array = { 1,2,3,4,5};
+// Console.WriteLine($"[{string.Join("; ", array)}]");
+
+
+//// 4. Вывод значения элемента под нужным индексом из переменной string в консоль
+// string test = "hello";
+// Console.WriteLine(test[1]);
+
+
+//// 4. вывод значения переменной strung в консоль
+// string test = "hello";
+// Console.WriteLine(test);
+
+
+//// 4. проверка значения переменой  test на то, что является ли она числом или нет
+// string test = "12345";
+// int result;
+// Console.WriteLine(int.TryParse(test, out result));
+// Console.WriteLine(test);
